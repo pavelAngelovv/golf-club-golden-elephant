5d4c6d18ce46c989dda37eb614ad06a5bb50bb91
Pavel Angelov <pangelov@codixis.com>
2026-03-12 14:22:58 +0200

    set up tournaments CPT with ACF date in functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,13 @@
 <?php
-function golf_club_setup() {
+function golf_club_setup()
+{
     add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_theme_support('html5', ['search-form', 'comment-form', 'gallery']);
+    add_theme_support('custom-logo');
 }
 add_action('after_setup_theme', 'golf_club_setup');
 
-function golf_club_enqueue_scripts() {
+function golf_club_enqueue_scripts()
+{
     wp_enqueue_style(
         'golf-club-main',
         get_template_directory_uri() . '/assets/css/main.css',
@@ -22,3 +23,57 @@ function golf_club_enqueue_scripts() {
     );
 }
 add_action('wp_enqueue_scripts', 'golf_club_enqueue_scripts');
+
+function golf_club_menus()
+{
+    register_nav_menus([
+        'primary' => 'Primary Menu',
+    ]);
+}
+add_action('after_setup_theme', 'golf_club_menus');
+
+function golf_club_register_cpt_tournament()
+{
+    $labels = [
+        'name' => 'Tournaments',
+        'singular_name' => 'Tournament',
+    ];
+
+    $args = [
+        'labels' => $labels,
+        'public' => true,
+        'rewrite' => ['slug' => 'tournaments'],
+        'has_archive' => false,
+        'menu_icon' => 'dashicons-awards',
+        'supports' => ['title'],
+        'show_in_rest' => true,
+    ];
+
+    register_post_type('tournament', $args);
+}
+add_action('init', 'golf_club_register_cpt_tournament');
+
+function golf_club_acf_fields()
+{
+    if (!function_exists('acf_add_local_field_group'))
+        return;
+
+    acf_add_local_field_group([
+        'key' => 'group_tournament',
+        'title' => 'Tournament Details',
+        'fields' => [
+            [
+                'key' => 'field_tournament_date',
+                'label' => 'Tournament Date',
+                'name' => 'tournament_date',
+                'type' => 'date_picker',
+                'display_format' => 'd/m/Y',
+                'return_format' => 'd/m/Y',
+            ],
+        ],
+        'location' => [
+            [['param' => 'post_type', 'operator' => '==', 'value' => 'tournament']],
+        ],
+    ]);
+}
+add_action('acf/init', 'golf_club_acf_fields');
